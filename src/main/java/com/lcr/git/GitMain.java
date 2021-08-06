a753c492cb3882cffdbaa8dbe6f370bbe3a52af6
lcr <1591311356@qq.com>
2021-08-06 18:19:22 +0800

    second commit-second

--- src/main/java/com/lcr/git/GitMain.java
+++ src/main/java/com/lcr/git/GitMain.java
@@ -3,5 +3,6 @@ package com.lcr.git;
 public class GitMain {
     public static void main(String[] args) {
         System.out.println("hello!!!!!!!!!!!!!!!!!!!");
+        System.out.println("hello!!!!!!!!!!!!!!!!!!!");
     }
 }
