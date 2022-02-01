acf17ab2cb34769959064796fd60b7f18a2d1f76
pourry <82203308+pourry@users.noreply.github.com>
2022-02-01 13:13:03 +0800

    Update Test2.java

--- src/main/java/com/lcr/git/Test2.java
+++ src/main/java/com/lcr/git/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
         System.out.println("hello git!!!");
         System.out.println("111111111111111");
         System.out.println("111111111111111");
+        System.out.println("2222222222222");
 
     }
 }
