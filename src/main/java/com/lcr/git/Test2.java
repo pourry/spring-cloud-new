28397c4238c69f6b388ccc361a7819ed0eaee7e3
lcr <1591311356@qq.com>
2022-02-01 12:48:18 +0800

    new-1

--- src/main/java/com/lcr/git/Test2.java
+++ src/main/java/com/lcr/git/Test2.java
@@ -4,5 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git!!!");
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+
     }
 }
